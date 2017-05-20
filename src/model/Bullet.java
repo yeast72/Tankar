5329f31cd2a9dba86cd359c6def3eafb2a42d359
Piromsurang <Piromsurang@Piromsurangs-MacBook-Pro.local>
2017-05-20 23:51:04 +0900

    created bullet pool

--- src/model/Bullet.java
+++ src/model/Bullet.java
@@ -3,10 +3,28 @@ package model;
 public class Bullet {
 	private int xPos;
 	private int yPos;
+	private long firedAt;
+	private boolean active;
+
 
 	public Bullet(int xPos, int yPos){
+		firedAt = System.currentTimeMillis();
+		active = true;
 		this.xPos = xPos;
 		this.yPos = yPos;
+	}
+	
+	public boolean hitWall() {
+		long time = System.currentTimeMillis();
+		return time - firedAt > 1000;
+	}
+	
+	public void deactivate() {
+		active = false;
+	}
+	
+	public boolean isActive() {
+		return active;
 	}
 	
 	public boolean hitEnermy(int enermyXPos, int enermyYPos){
