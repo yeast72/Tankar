eb66d847098a4380e54581fb4d1485af2d098963
Piromsurang <Piromsurang@Piromsurangs-MacBook-Pro.local>
2017-05-20 23:45:45 +0900

    created singleton Game layout in model package

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,15 +1,126 @@
 package model;
 
-public class Game {
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
-	public static Game getInstance() {
-		// TODO Auto-generated method stub
-		return null;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 300;
+	public static final int HEIGHT = WIDTH / 12 * 9;
+	public static final int SCALE = 3;
+	public static final String NAME = "Tankar.io";
+	
+	private JFrame frame;
+	
+	public boolean running;
+	public int tickCount = 0;
+	
+	private static Game instance = null;
+	
+	private Game() {
+		setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		setMaximumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		
+		frame = new JFrame(NAME);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+		frame.add(this, BorderLayout.CENTER);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
-	public void start() {
-		// TODO Auto-generated method stub
+	public static Game getInstance() {
 		
+		if(instance == null) {
+			if(instance == null) {
+				instance = new Game();
+			}
+		}
+		
+		return instance;
+	}
+
+	public synchronized void start() {
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public synchronized void stop() {
+		running = false;
+	}
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		double nsPerTick = 1000000000D/60D;
+		
+		int ticks = 0;
+		int frames = 0;
+		
+		long lastTimer = System.currentTimeMillis();
+		double delta = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean shouldRender = true;
+			while(delta >= 1) {
+				ticks++;
+				tick();
+				delta -= 1;
+			}
+			
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if(shouldRender) {
+				frames++;
+				render();
+			}
+			
+			if(System.currentTimeMillis() - lastTimer >= 1000) {
+				lastTimer += 1000;
+				System.out.println(ticks + " ticks, " + frames + " frames");
+				frames = 0;
+				ticks = 0;
+			}
+		}
+	}
+	
+	public void tick() {
+		tickCount++;
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+	
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String[] args) {
+		Game game = Game.getInstance();
+		game.start();
 	}
 
 }
